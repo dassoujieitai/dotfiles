497df01483a22b9073cf6688e3ceb0669aeeda07
dassoujieitai <dassoujieitai@gmail.com>
2015-02-05 23:26:50 +0900

    cleanup unnecessary files.

--- .emacs.d/conf/02-anthy.el
+++ .emacs.d/conf/02-anthy.el
@@ -1,4 +1,4 @@
-;; Anthyの設定                                                                  
+;; Anthyの設定
 (when (eq system-type 'darwin)
   (add-to-list 'exec-path "/Users/masa/util/anthy/anthy-9100h/bin"))
 
@@ -7,26 +7,25 @@
   (load-file "/Users/masa/.emacs.d/elisp/anthy/leim-list.el"))
 (setq default-input-method 'japanese-anthy)
 
-;; かな入力の設定。                                                             
+;; かな入力の設定。
 (anthy-kana-map-mode)
 
-;; C-oで日本語の入力モードをtoggle                                              
+;; C-oで日本語の入力モードをtoggle
 (define-key global-map (kbd "C-o") 'toggle-input-method)
 
-;; かな漢字入力モードでSpaceを押したとき、半角空白が挿入されるようにする。      
+;; かな漢字入力モードでSpaceを押したとき、半角空白が挿入されるようにする。
 (setq anthy-wide-space " ")
 
-(anthy-change-hiragana-map "4@" "ヴ")
+(when (eq system-type 'darwin)
+  (define-key global-map [?¥] [?\\]))  ;; ¥の代わりにバックスラッシュを入力する
 
-(anthy-change-hiragana-map "¥" "ー")
-(anthy-change-hiragana-map "\\" "ー")
-
-; (if (eq system-type 'darwin)
-;    (anthy-change-hiragana-map "¥¥" "ー")
-;  (anthy-change-hiragana-map "\\" "ー"))
-
-(anthy-change-hiragana-map "`"  "ろ")
-(anthy-change-hiragana-map "|"  "ー")
-(anthy-change-hiragana-map "{"  "「")
-(anthy-change-hiragana-map "}"  "」")
-(anthy-change-hiragana-map "?"  "・")
+(when (eq system-type 'darwin)
+  (anthy-change-hiragana-map "4@" "ヴ")
+  (anthy-change-hiragana-map "¥¥" "ー")
+  (anthy-change-hiragana-map "\\" "ー")
+  (anthy-change-hiragana-map "`"  "ろ")
+  (anthy-change-hiragana-map "|"  "を")
+  (anthy-change-hiragana-map "{"  "「")
+  (anthy-change-hiragana-map "}"  "」")
+  (anthy-change-hiragana-map "?"  "・")
+  (anthy-change-hiragana-map "~"  "を"))
