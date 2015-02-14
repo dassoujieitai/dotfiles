4a0ee1484703aae259a7587e3d5e91569bac295e
dassoujieitai <dassoujieitai@gmail.com>
2015-02-14 23:23:52 +0900

    add how to install for some add-on packages.

--- .emacs.d/conf/06-lispxmp.el
+++ .emacs.d/conf/06-lispxmp.el
@@ -1,5 +1,10 @@
 ;; lispxmp.elの設定。
 
+;; インストール
+;;
+;;     M-x install-elisp-from-emacswiki lispxmp.el
+;;
+
 ;; 式の評価結果を注釈するための設定。
 (require 'lispxmp)
 
--- .emacs.d/conf/07-paredit.el
+++ .emacs.d/conf/07-paredit.el
@@ -1,6 +1,9 @@
 ;; paredit.el用の設定。
+
+;; インストール
 ;;
-;; paredit.elのインストールコマンド。
+;;     M-x install-elisp http://mumble.net/~campbell/emacs/paredit.el
+;;
 
 ;; 括弧の対応を保持して編集する設定。
 ;; (require 'paredit)
@@ -9,5 +12,3 @@
 ;; (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
 ;; (add-hook 'lisp-mode-hook 'enable-paredit-mode)
 ;; (add-hook 'ielm-mode-hook 'enable-paredit-mode)
-
-
