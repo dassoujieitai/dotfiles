73bbedf18afe2e4a5de31c00ba780e8fc896003c
dassoujieitai <dassoujieitai@gmail.com>
2018-11-25 16:53:39 +0900

    add auto load configuration. and custom configuration file.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,7 @@
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
             (normal-top-level-add-subdirs-to-load-path))))))
 
-(add-to-load-path "elisp" "conf" "public_repos")
+(add-to-load-path "elisp" "conf" "public_repos" "auto-install")
 
 (setq custom-file (locate-user-emacs-file "custom.el"))
 
