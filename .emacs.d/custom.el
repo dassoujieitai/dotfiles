94fdefdcb18435420b1a1750434771c3832711ef
dassoujieitai <dassoujieitai@gmail.com>
2018-12-16 14:11:43 +0900

    auto generated custom.el.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,10 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
+    ("39546362fed4d5201b2b386dc21f21439497c9eec5fee323d953b3e230e4083e" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
  '(package-selected-packages
    (quote
-    (mozc flycheck-pos-tip flycheck inf-ruby ruby-electric web-mode auto-complete mew wanderlust atom-dark-theme atom-one-dark-theme helm-descbinds helm zenburn-theme))))
+    (twittering-mode google-maps inverse-acme-theme auto-async-byte-compile paredit lispxmp open-junk-file mozc flycheck-pos-tip flycheck inf-ruby ruby-electric web-mode auto-complete mew wanderlust atom-dark-theme atom-one-dark-theme helm-descbinds helm zenburn-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
